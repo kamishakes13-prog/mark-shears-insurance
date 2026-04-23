3a8c4b123e1e27cba92aeb900323ade8e319ab58
kamishakes13-prog <kamishakes13@gmail.com>
2026-04-23 16:44:06 -0500

    Fix: Remove erroneous }> at start of Navbar.jsx import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-  }>{import { useState, useEffect } from 'react'
+  {import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { FaBars, FaTimes, FaPhone } from 'react-icons/fa'
 
